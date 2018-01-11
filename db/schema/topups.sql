4698022adfc05b77b33447325aafb45fa3ae0585
zander <aamagtipon@voyagerinnovation.com>
2018-01-11 08:59:40 +0800

    restructured topups table

--- db/schema/topups.sql
+++ db/schema/topups.sql
@@ -3,11 +3,11 @@ SET search_path TO public;
 DROP TABLE IF EXISTS topups CASCADE;
 CREATE TABLE IF NOT EXISTS topups (
   id                 uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+  request_id         uuid NOT NULL UNIQUE,
   wallet_id          uuid NOT NULL REFERENCES wallets(id),
   amount             decimal(19,4) NOT NULL,
+  source             text NOT NULL,
   remarks            text,
-  current_balance    decimal(19,4) NOT NULL,
-  available_balance  decimal(19,4) NOT NULL,
   created_at         timestamp with time zone NOT NULL DEFAULT now(),
   updated_at         timestamp with time zone NOT NULL DEFAULT now()
 );
